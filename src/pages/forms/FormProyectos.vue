ec2bf037a4910f47ffc4f77671a0cb05fbd1b3e9
Debora MR <dmedina@iesrafaelalberti.es>
2024-06-08 21:03:13 +0200

    feat: Inclusion de 'Añadir' y 'Eliminar' en Proyectos

--- src/pages/forms/FormProyectos.vue
+++ src/pages/forms/FormProyectos.vue
@@ -1,12 +1,22 @@
 <script setup>
 
 import {useUserStore} from "../../stores/UserStore.js";
+import {useSelectStore} from "../../stores/SelectStore.js";
 import {onMounted, ref} from "vue";
 import axios from "axios";
 const userStore = useUserStore();
+const selectStore = useSelectStore();
 
 // Definir una ref para almacenar los proyectos
 let projects = ref([]);
+let newProject = ref({
+  title: '',
+  description: '',
+  img_url: '',
+  technologies: []
+});
+
+let showForm = ref(false);
 
 // Función para obtener los proyectos del usuario
 const fetchProjects = async () => {
@@ -21,35 +31,126 @@ const fetchProjects = async () => {
   }
 };
 
+const handleFileUpload = (event) => {
+  newProject.value.img_url = event.target.files[0];
+};
+
+const addProject = async () => {
+  const config = {
+    headers: {Authorization: `Bearer ${userStore.token}`, 'Content-Type': 'multipart/form-data'}
+  };
+  try {
+    const technologiesArray = newProject.value.technologies.map((tech) => ({
+      name: tech
+    }));
+
+    const formData = new FormData();
+    formData.append('img_url', newProject.value.img_url);
+    formData.append('project_data', JSON.stringify({
+      title: newProject.value.title,
+      description: newProject.value.description,
+      technologies: technologiesArray,
+    }));
+
+    const response = await axios.post('/api/v1/projects/', formData, config);
+
+    projects.value.push(response.data.project);
+
+    newProject.value = {
+      title: '',
+      description: '',
+      technologies: []
+    };
+
+    showForm.value = false;
+  } catch (error) {
+    console.error('Error al agregar el proyecto:', error);
+  }
+};
+
+const deleteProject = async (id) => {
+  const config = {
+    headers: { Authorization: `Bearer ${userStore.token}` }
+  };
+  try {
+    await axios.delete(`/api/v1/projects/${id}`, config);
+    projects.value = projects.value.filter(project => project.id !== id);
+  } catch (error) {
+    console.error('Error al eliminar el proyecto:', error);
+  }
+};
+
 // Usar el hook onMounted para llamar a la función cuando el componente se monte
 onMounted(() => {
   fetchProjects();
+  selectStore.fetchAllSelectOptionsEnums();
 });
 
 </script>
 
 <template>
 
-    <section v-if="projects">
+    <section>
       <h2>Proyectos</h2>
-        <div class="projects-section">
 
-          <div v-for="project in projects" :key="project.id" class="project">
-            <div class="project-content-image">
-              <img :src="project.image_url" alt="Imagen del proyecto" class="project-image">
-            </div>
+      <!-- Botón para mostrar el formulario -->
+      <button @click="showForm = true" v-if="!showForm">Agregar Proyecto</button>
 
-            <div class="project-info">
+      <!-- Formulario para agregar nueva experiencia, visible solo cuando showForm es true -->
+      <div class="new-project-form" v-if="showForm">
+        <h3>Agregar Nuevo Proyecto</h3>
+        <form @submit.prevent="addProject">
+          <label>
+            Título:
+            <input v-model="newProject.title" type="text" required/>
+          </label>
+          <label>
+            Descripción:
+            <input v-model="newProject.description" type="text" required/>
+          </label>
+          <label>
+            Tecnologías:
+            <select v-model="newProject.technologies" multiple>
+              <option
+                  v-for="option in selectStore.technologiesSelectData"
+                  :key="option.value"
+                  :value="option.value">{{ option.label }}
+              </option>
+            </select>
+          </label>
+          <label>
+            Avatar
+            <input type="file"
+                   id="avatar"
+                   @change="handleFileUpload"/>
+          </label>
+          <button type="submit">Agregar Proyecto</button>
+          <button type="button" @click="showForm = false">Cancelar</button>
+        </form>
+      </div>
+
+      <div class="projects-section">
+
+        <div v-for="project in projects.slice().reverse()" :key="project.id" class="project">
+          <div class="project-content-image">
+            <img :src="project.image_url" alt="Imagen del proyecto" class="project-image">
+          </div>
+
+          <div class="project-info">
+            <div class="project-header">
               <h2>{{project.title}}</h2>
-              <p>{{project.description}}</p>
-              <p><strong>Tecnologías:</strong>
-                <span v-for="technology in project.technologies" :key="technology.name" class="project-info-tecnologias">
-                  {{technology.name}}
-                </span>
-              </p>
+              <button class="delete-button" @click="deleteProject(project.id)"><font-awesome-icon :icon="['fas', 'trash']"/></button>
             </div>
+
+            <p>{{project.description}}</p>
+            <p><strong>Tecnologías:</strong>
+              <span v-for="technology in project.technologies" :key="technology.name" class="project-info-tecnologias">
+                {{technology.name}}
+              </span>
+            </p>
           </div>
         </div>
+      </div>
     </section>
 </template>
 
@@ -130,5 +231,58 @@ h2 {
   border-radius: 30px;
   padding: 5px 10px;
   margin: 5px;
+}
+
+.project-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.new-project-form {
+  margin-top: 30px;
+}
+
+.new-project-form form {
+  display: flex;
+  flex-direction: column;
+}
+
+.new-project-form label {
+  margin-bottom: 10px;
+}
+
+.new-project-form input,
+.new-project-form textarea {
+  margin-bottom: 10px;
+  padding: 8px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+}
+
+.new-project-form button {
+  padding: 10px;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-right: 10px;
+}
+
+.new-project-form button:hover {
+  background-color: #45a049;
+}
+
+.delete-button {
+  background: none;
+  border: none;
+  color: #c00;
+  cursor: pointer;
+  font-size: 1.2em;
+}
+
+.delete-button:hover {
+  color: #f00;
 }
 </style>
